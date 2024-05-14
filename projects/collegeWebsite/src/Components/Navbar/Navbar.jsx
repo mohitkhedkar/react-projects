0b576b4ca2c9b50596c504984fcc6242258d74f5
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-05-14 18:14:04 +0530

    :sparkles: edusity college website features added

--- projects/collegeWebsite/src/Components/Navbar/Navbar.jsx
+++ projects/collegeWebsite/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 700 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="container">
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
