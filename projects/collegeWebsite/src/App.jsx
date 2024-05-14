85f4174ddef2621362b7be0c7d0ebebee81e5341
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-05-14 19:25:36 +0530

    :sparkles: edusity college website testimonial feature added

--- projects/collegeWebsite/src/App.jsx
+++ projects/collegeWebsite/src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Title subTitle="TESTIMONIALS" title="What Student Says" />
         <Testimonials />
         <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
       </div>
     </>
   );
