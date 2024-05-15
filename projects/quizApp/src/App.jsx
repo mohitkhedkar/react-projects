2a0a4686950a2a2bd4e03e99dba24526a3d88377
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-05-16 01:34:33 +0530

    Quiz App Added

---
+++ projects/quizApp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Quiz from "./Components/Quiz/Quiz";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App;
