cb736c9be395f3967d33cf43106f306f1e06a63a
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-08-10 02:45:06 +0530

    stickyNotes functionality added

--- projects/stickyNotesApp/src/components/NoteCard.jsx
+++ projects/stickyNotesApp/src/components/NoteCard.jsx
@@ -1,25 +1,87 @@
 import React from "react";
 import Trash from "../icons/Trash";
-import { useRef, useEffect } from "react";
+import Spinner from "../icons/Spinner";
+import { useRef, useEffect, useState } from "react";
+import { setNewOffset, autoGrow, setZIndex, bodyParser } from "../utils";
+import { db } from "../appwrite/databases";
+import DeleteButton from "../components/DeleteButton";
 
 const NoteCard = ({ note }) => {
-  let position = JSON.parse(note.position);
+  const [saving, setSaving] = useState(false);
+  const keyUpTimer = useRef(null);
+
+  const [position, setPositon] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
-  const body = JSON.parse(note.body);
+  const body = bodyParser(note.body);
   const textAreaRef = useRef(null);
+
+  let mouseStartPos = { x: 0, y: 0 };
+
+  const cardRef = useRef(null);
 
   useEffect(() => {
     autoGrow(textAreaRef);
   }, []);
 
-  function autoGrow(textAreaRef) {
-    const { current } = textAreaRef;
-    current.style.height = "auto";
-    current.style.height = current.scrollHeight + "px";
-  }
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    if (e.target.className === "card-header") {
+      setZIndex(cardRef.current);
+      document.addEventListener("mousemove", mouseMove);
+      document.addEventListener("mouseup", mouseUp);
+    }
+  };
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+
+    const newPosition = setNewOffset(cardRef.current); //{x,y}
+    db.notes.update(note.$id, { position: JSON.stringify(newPosition) });
+    saveData("position", newPosition);
+  };
+
+  const saveData = async (key, value) => {
+    const payload = { [key]: JSON.stringify(value) };
+    try {
+      await db.notes.update(note.$id, payload);
+    } catch (error) {
+      console.error(error);
+    }
+    setSaving(false);
+  };
+
+  const handleKeyUp = async () => {
+    setSaving(true);
+
+    if (keyUpTimer.current) {
+      clearTimeout(keyUpTimer.current);
+    }
+
+    keyUpTimer.current = setTimeout(() => {
+      saveData("body", textAreaRef.current.value);
+    }, 2000);
+  };
+
+  const mouseMove = (e) => {
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+    setPosition(newPosition);
+
+    setPositon(newPosition);
+  };
 
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -28,12 +90,25 @@ const NoteCard = ({ note }) => {
       }}
     >
       <div
+        onMouseDown={mouseDown}
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
-      ></div>
+      >
+        <DeleteButton noteId={note.$id} setNotes={setNotes} />
+      </div>
       <Trash />
+      {saving && (
+        <div className="card-saving">
+          <Spinner color={colors.colorText} />
+          <span style={{ color: colors.colorText }}>Saving...</span>
+        </div>
+      )}
       <div className="card-body">
         <textarea
+          onKeyUp={handleKeyUp}
+          onFocus={() => {
+            setZIndex(cardRef.current);
+          }}
           ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={body}
--- projects/stickyNotesApp/src/pages/NotesPage.jsx
+++ projects/stickyNotesApp/src/pages/NotesPage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NoteCard from "../components/NoteCard";
-import { data as notes } from "../assets/data";
+// import { data as notes } from "../assets/data";
+import { databases } from "../appwrite/config";
+import { db } from "../appwrite/databases";
 
 const NotesPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const response = await db.notes.list();
+    setNotes(response.documents);
+  };
+
   return (
     <div>
       {notes.map((note) => (
