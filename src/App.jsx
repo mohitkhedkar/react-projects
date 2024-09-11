e1cd36ea109bb06115a1835a6f8620a390e61448
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-09-11 05:52:09 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import "./App.css";
 import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
